43152a5aea13ce171289bc274ca16be19acc4d7c
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-18 23:41:39 +0200

    changed lint-staged command

--- dist/_virtual/_virtual_multi-entry.js
+++ dist/_virtual/_virtual_multi-entry.js
@@ -1,32 +1,32 @@
 'use strict';
 
 var index = require('../index.js');
-var CollectionType = require('../enums/CollectionType.js');
 var Endian = require('../enums/Endian.js');
+var CollectionType = require('../enums/CollectionType.js');
 var VarType = require('../enums/VarType.js');
 var BufferReader = require('../utils/BufferReader.js');
+var StructBuilder = require('../utils/StructBuilder.js');
 var BufferWriter = require('../utils/BufferWriter.js');
 var TypeSizes = require('../utils/TypeSizes.js');
-var StructBuilder = require('../utils/StructBuilder.js');
 
 
 
 exports.MappedBuffer = index.MappedBuffer;
-Object.defineProperty(exports, "CollectionType", {
-	enumerable: true,
-	get: function () { return CollectionType.CollectionType; }
-});
 Object.defineProperty(exports, "Endian", {
 	enumerable: true,
 	get: function () { return Endian.Endian; }
+});
+Object.defineProperty(exports, "CollectionType", {
+	enumerable: true,
+	get: function () { return CollectionType.CollectionType; }
 });
 Object.defineProperty(exports, "VarType", {
 	enumerable: true,
 	get: function () { return VarType.VarType; }
 });
 exports.BufferReader = BufferReader.BufferReader;
+exports.StructBuilder = StructBuilder.StructBuilder;
 exports.BufferWriter = BufferWriter.BufferWriter;
 exports.calculateArraySize = TypeSizes.calculateArraySize;
 exports.calculateStructSize = TypeSizes.calculateStructSize;
 exports.getVarTypeSize = TypeSizes.getVarTypeSize;
-exports.StructBuilder = StructBuilder.StructBuilder;
--- dist/_virtual/_virtual_multi-entry.mjs
+++ dist/_virtual/_virtual_multi-entry.mjs
@@ -1,8 +1,8 @@
 export { MappedBuffer } from '../index.mjs';
-export { CollectionType } from '../enums/CollectionType.mjs';
 export { Endian } from '../enums/Endian.mjs';
+export { CollectionType } from '../enums/CollectionType.mjs';
 export { VarType } from '../enums/VarType.mjs';
 export { BufferReader } from '../utils/BufferReader.mjs';
+export { StructBuilder } from '../utils/StructBuilder.mjs';
 export { BufferWriter } from '../utils/BufferWriter.mjs';
 export { calculateArraySize, calculateStructSize, getVarTypeSize } from '../utils/TypeSizes.mjs';
-export { StructBuilder } from '../utils/StructBuilder.mjs';
