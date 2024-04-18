b63e0804f220eaee59c7c72b50bd9a41dc96b376
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-18 23:06:16 +0200

    updated unit test

--- dist/_virtual/_virtual_multi-entry.js
+++ dist/_virtual/_virtual_multi-entry.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var index = require('../index.js');
-var CollectionType = require('../enums/CollectionType.js');
 var Endian = require('../enums/Endian.js');
 var VarType = require('../enums/VarType.js');
+var CollectionType = require('../enums/CollectionType.js');
 var BufferReader = require('../utils/BufferReader.js');
 var StructBuilder = require('../utils/StructBuilder.js');
 var BufferWriter = require('../utils/BufferWriter.js');
@@ -11,10 +11,6 @@ var BufferWriter = require('../utils/BufferWriter.js');
 
 
 exports.MappedBuffer = index.MappedBuffer;
-Object.defineProperty(exports, "CollectionType", {
-	enumerable: true,
-	get: function () { return CollectionType.CollectionType; }
-});
 Object.defineProperty(exports, "Endian", {
 	enumerable: true,
 	get: function () { return Endian.Endian; }
@@ -22,6 +18,10 @@ Object.defineProperty(exports, "Endian", {
 Object.defineProperty(exports, "VarType", {
 	enumerable: true,
 	get: function () { return VarType.VarType; }
+});
+Object.defineProperty(exports, "CollectionType", {
+	enumerable: true,
+	get: function () { return CollectionType.CollectionType; }
 });
 exports.BufferReader = BufferReader.BufferReader;
 exports.StructBuilder = StructBuilder.StructBuilder;
--- test/test_binding.js
+++ test/test_binding.js
@@ -7,12 +7,16 @@ assert(StructBuilder, 'The StructBuilder is undefined')
 assert(MappedBuffer, 'The MappedBuffer is undefined')
 
 // Create a test struct
-const test3 = new StructBuilder().addVariable('a', VarType.int).build()
+const test3 = new StructBuilder()
+  .addVariable('a', VarType.int)
+  .build()
+
 const test2 = new StructBuilder()
   .addVariable('a', VarType.int)
   .addStruct('b', test3)
   .addArray('c', VarType.float, 2)
   .build()
+
 const test = new StructBuilder()
   .addVariable('a', VarType.bool)
   .addArray('b', StructBuilder.createStruct(test2), 5)
@@ -26,30 +30,30 @@ const instance = new MappedBuffer('test', test)
 
 // Validate all functions
 assert.doesNotThrow(() => {
-  assert(instance.create, 'create function is undefined')
-  assert(instance.open, 'open function is undefined')
-  assert(instance.read, 'read function is undefined')
-  assert(instance.write, 'write function is undefined')
-  assert(instance.close, 'close function is undefined')
-  assert(MappedBuffer.getVarTypeSize, 'getVarTypeSize function is undefined')
-  assert(MappedBuffer.calculateStructSize, 'calculateStructSize function is undefined')
-  assert(MappedBuffer.calculateArraySize, 'calculateArraySize function is undefined')
+  assert(instance.create, 'Create function is undefined')
+  assert(instance.open, 'Open function is undefined')
+  assert(instance.read, 'Read function is undefined')
+  assert(instance.write, 'Write function is undefined')
+  assert(instance.close, 'Close function is undefined')
+  assert(MappedBuffer.getVarTypeSize, 'GetVarTypeSize function is undefined')
+  assert(MappedBuffer.calculateStructSize, 'CalculateStructSize function is undefined')
+  assert(MappedBuffer.calculateArraySize, 'CalculateArraySize function is undefined')
 }, 'Not all functions are defined')
 
 // Create buffer for testing
-assert.doesNotThrow(() => instance.create(), "create did throw an exception")
+assert.doesNotThrow(() => instance.create(), "Create did throw an exception")
 
 // Test weather read returns struct
-assert.notStrictEqual(instance.read(), undefined, "read didn't return a value");
+assert.notStrictEqual(instance.read(), undefined, "Read didn't return a value");
 
 // Test weather write works
 let data = instance.read()
 data['a'] = true
-assert.doesNotThrow(() => instance.write(data), "write did throw an exception")
+assert.doesNotThrow(() => instance.write(data), "Write did throw an exception")
 data = instance.read()
 assert.strictEqual(data['a'], true, "Changed value is not correct");
 
 // Test weather close works
-assert.doesNotThrow(() => instance.close(), "close did throw an exception")
+assert.doesNotThrow(() => instance.close(), "Close did throw an exception")
 
-console.log('Tests passed- everything looks OK!')
+console.log('Tests passed - everything looks OK!')
