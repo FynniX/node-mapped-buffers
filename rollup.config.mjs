58a6d248c0246495ef41934be5667b3386d5745d
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-20 02:59:27 +0200

    updated rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,18 +6,20 @@ import eslint from '@rollup/plugin-eslint'
 
 export default {
   input: 'lib/**/*.ts',
-  external: ['fs'],
+  external: ['fs', 'path', 'antlr4'],
   output: [
     {
       dir: 'dist',
       format: 'cjs',
       preserveModules: true,
+      exports: 'named',
     },
     {
       dir: 'dist',
       format: 'es',
       entryFileNames: '[name].mjs',
       preserveModules: true,
+      exports: 'named',
     }
   ],
   plugins: [
