e810c8b11e2231dccdbae2e3d9fcffcc69c2d68a
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-04-20 02:33:43 +0200

    added scripts and deps

--- dist/_virtual/_virtual_multi-entry.js
+++ dist/_virtual/_virtual_multi-entry.js
@@ -1,27 +1,23 @@
 'use strict';
 
 var index = require('../index.js');
-var PrimitiveType = require('../enums/PrimitiveType.js');
 var CollectionType = require('../enums/CollectionType.js');
 var DeclarationType = require('../enums/DeclarationType.js');
-var VarType = require('../enums/VarType.js');
 var Endian = require('../enums/Endian.js');
-var BufferReader = require('../utils/BufferReader.js');
+var VarType = require('../enums/VarType.js');
+var PrimitiveType = require('../enums/PrimitiveType.js');
 var BufferWriter = require('../utils/BufferWriter.js');
-var StructBuilder = require('../utils/StructBuilder.js');
+var BufferReader = require('../utils/BufferReader.js');
 var SchemaReader = require('../utils/SchemaReader.js');
+var StructBuilder = require('../utils/StructBuilder.js');
 var TypeSizes = require('../utils/TypeSizes.js');
-require('antlr4');
-var SchemaParser = require('../utils/parser/SchemaParser.js');
 require('../utils/parser/SchemaLexer.js');
+var SchemaParser = require('../utils/parser/SchemaParser.js');
+require('antlr4');
 
 
 
 exports.MappedBuffer = index.MappedBuffer;
-Object.defineProperty(exports, "PrimitiveType", {
-	enumerable: true,
-	get: function () { return PrimitiveType.PrimitiveType; }
-});
 Object.defineProperty(exports, "CollectionType", {
 	enumerable: true,
 	get: function () { return CollectionType.CollectionType; }
@@ -30,18 +26,22 @@ Object.defineProperty(exports, "DeclarationType", {
 	enumerable: true,
 	get: function () { return DeclarationType.DeclarationType; }
 });
-Object.defineProperty(exports, "VarType", {
-	enumerable: true,
-	get: function () { return VarType.VarType; }
-});
 Object.defineProperty(exports, "Endian", {
 	enumerable: true,
 	get: function () { return Endian.Endian; }
 });
-exports.BufferReader = BufferReader.BufferReader;
+Object.defineProperty(exports, "VarType", {
+	enumerable: true,
+	get: function () { return VarType.VarType; }
+});
+Object.defineProperty(exports, "PrimitiveType", {
+	enumerable: true,
+	get: function () { return PrimitiveType.PrimitiveType; }
+});
 exports.BufferWriter = BufferWriter.BufferWriter;
-exports.StructBuilder = StructBuilder.StructBuilder;
+exports.BufferReader = BufferReader.BufferReader;
 exports.SchemaReader = SchemaReader.SchemaReader;
+exports.StructBuilder = StructBuilder.StructBuilder;
 exports.calculateArraySize = TypeSizes.calculateArraySize;
 exports.calculateStructSize = TypeSizes.calculateStructSize;
 exports.getVarTypeSize = TypeSizes.getVarTypeSize;
